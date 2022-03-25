bcbdf2e7b3730fe5b625dbae426a18221d88e34c
prubioamzubiri <prubioam@zubirimanteo.com>
2022-03-25 08:33:41 +0000

    Creado GestorCarrearas

--- src/main/java/dominio/Carrera.java
+++ src/main/java/dominio/Carrera.java
@@ -2,24 +2,38 @@ package dominio;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-
+@Entity
+@Table(name="Carrera")
 public class Carrera {
 	
+	@Id
+	private String id;
 
 	private String nombre_gp;
 
+	@ManyToOne()
+	@JoinColumn(name ="Circuito_id")
 	private Circuito circuito;
 	
 	private Date fecha_gp;
 	
 	private int ano;
 	
-	private HashMap<Integer, Piloto> pilotos_por_posicion;
+	@ElementCollection
+    @CollectionTable(name = "posicion_en_carrera", 
+      joinColumns = {@JoinColumn(name = "posicion_id", referencedColumnName = "id")})
+    @MapKeyColumn(name = "posición")
+    @Column(name = "piloto")
+	private Map<Integer, Piloto> pilotos_por_posicion;
 	
 	public Carrera(String nombre_gp, Date fecha_gp, Circuito circuito, int ano)
 	{
@@ -36,3 +50,4 @@ public class Carrera {
 		pilotos_por_posicion = new HashMap<Integer, Piloto>();
 	}
 }
+
